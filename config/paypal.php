d093eafacbae4aead60ac29f254c91c5d10e5edf
prasunmatrix <prasun@matrixnmedia.com>
2023-02-03 18:46:28 +0530

    first commit

---
+++ config/paypal.php
@@ -0,0 +1,12 @@
+<?php
+return [
+  'client_id' => 'AWBLEnsv4IUNkQHjc3_-vAr_aFUVDn_XWfL-tDon2nQtChobvW7KzMcltHDyxzhrYegYkQjlVoyj3qBo',
+  'secret' => 'EFxSqk0APKqwzI2A_xtEsF55W6hRE1JLOn4_MiW74E7XKvA9R2nBN2nCRXkxvQkeYj9QPP-C5MAIJxW0',
+  'settings' => array(
+    'mode' => 'sandbox',
+    'http.ConnectionTimeOut' => 1000,
+    'log.LogEnabled' => true,
+    'log.FileName' => storage_path() . '/logs/paypal.log',
+    'log.LogLevel' => 'FINE'
+  ),
+];
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+use App\Http\Controllers\PaypalController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//Route::get('paywithpaypal', array('as' => 'paywithpaypal','uses' => 'PaypalController@payWithPaypal',));
+// Route::post('paypal', array('as' => 'paypal','uses' => 'PaypalController@postPaymentWithpaypal',));
+// Route::get('paypal', array('as' => 'status','uses' => 'PaypalController@getPaymentStatus',));
+
+Route::get('/paywithpaypal', [PaypalController::class, 'paywithpaypal'])->name('paywithpaypal');
+Route::post('paypal', [PaypalController::class, 'postPaymentWithpaypal'])->name('paypal');
+Route::get('paypal', [PaypalController::class, 'getPaymentStatus'])->name('status');
+ 
+
